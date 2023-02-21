abc16b483e4e2a72c32f73562cb0478a7b91b628
Antonio0806 <antonio0806123@gmail.com>
2023-02-21 14:29:14 +0100

    Removed secrets bruv

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import BottomMenu from "./BottomMenu";
 const products = [
     {
         id: 1,
-        name: 'Kolaż (wydruk) "Spokój" w formacie A3',
+        name: 'Product 1',
         image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.ditdXPVVQ8FN87MLTGMbqQHaFf%26pid%3DApi&f=1&ipt=8c6fd9a972fa6a10253bc875937f8d3dd7df2d9b8343053d66ad7404c42a2f27&ipo=images',
         price: '$199',
-        description: 'Kolaż przedstawia ptaka - łabędzia, żyjącego z kompletami zastawy i obrusami na falach jednego stawu. Jak wszystkie babcie spokojny, słychać nawet tykanie zegara. Autorka pracy - Siliakova Maryna. Artystka urodziłą się w 1992 roku w Doniecku w Ukrainie, do Polski przeprowadziła się w 2017 roku. Z wykształcenia chemiczka, w Warszawie postanowiła się przekwalifikować - obecnie jest absolwentką projektowania graficznego Społecznej Akademii Nauk. Artystka tworzy zarówno kolaże cyfrowe, jak i analogowe oraz chętnie dzieli się zdobytą wiedzą z otoczeniem, Maryna Siliakova jest młodą artystką nastawioną na poszukiwanie i odkrywanie, o czym świadczy eksploracja przez nią różnych technik, stosowanie rozmaitych materiałów od pasteli po tusz i eksperymentalne podejście do kompozycji. Jej prace były wyróżnianie w wielu konkursach krajowych i międzynarodowych.',
+        description: 'test',
     },
     {
         id: 2,
